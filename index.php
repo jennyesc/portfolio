6536be346fe5ce5bd35c8658bda202f5a4494ddb
jennyesc <71670394+jennyesc@users.noreply.github.com>
2021-03-01 23:52:41 -0800

    Styling for mobile (home)

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
     <header>
         <?php include 'header.php';?>
     </header>
-
+    
     <main>
         <section id="top" class="intro-container">
             <div class="intro">
@@ -45,61 +45,63 @@
             </div>
         </section>
 
-        <section class="current-container">
-            <picture>
-                <source media="(max-width: 500px)" srcset="images/two-sisters-mobile-500.jpg">
-                <source media="(max-width: 750px)" srcset="images/two-sisters-mobile-1000.jpg">
-                <source media="(min-width: 750px)" srcset="images/two-sisters-mobile-1200ls.jpg">
-                <img src="images/two-sisters-mobile-1000.jpg" alt="two iPhones on a red background">
-			</picture>
+        <div class="wrapper-desktop">
+            <section class="current-container">
+                <picture class="cur-img">
+                    <source media="(max-width: 500px)" srcset="images/two-sisters-mobile-500.jpg">
+                    <source media="(max-width: 750px)" srcset="images/two-sisters-mobile-1000.jpg">
+                    <source media="(min-width: 750px)" srcset="images/two-sisters-mobile-1200ls.jpg">
+                    <img src="images/two-sisters-mobile-1000.jpg" alt="two iPhones on a red background">
+                </picture>
 
-            <section class="cur-text-container">
-                <h3>Current Project</h3>
-                <h4>Two Sisters Bakery</h4>
-                <p class="dev-par"> In Development</p>
+                <section class="cur-text-container">
+                    <h3>Current Project</h3>
+                    <h4>Two Sisters Bakery</h4>
+                    <p class="dev-par"> In Development</p>
 
-                <h4 class="tools"> Tools</h4>
-                <p class="tools-par"> Adobe XD | Adobe Photoshop <br> WordPress | WooCommerce</p>
+                    <h4 class="tools"> Tools</h4>
+                    <p class="tools-par"> Adobe XD | Adobe Photoshop <br> WordPress | WooCommerce</p>
 
-                <div class=btn-container>
-                    <a class="red-btn faux-btn" href="two-sisters.php"> Go to Project</a>
-                </div>
+                    <div class=btn-container>
+                        <a class="red-btn faux-btn" href="two-sisters.php"> Go to Project</a>
+                    </div>
+
+                </section>
+
+                <article class="view-all-btn">
+                    <a href="projects.php" class="view-all-link">View all projects</a>
+                </article>
 
             </section>
 
-            <article class="view-all-btn">
-                <a href="projects.php" class="view-all-link">View all projects</a>
+            <section class="about-jenny-container">
+
+                <picture class="jne-pic">
+                    <source media="(max-width: 500px)" srcset="images/about-jenny-320w.jpg">
+                    <source media="(max-width: 750px)" srcset="images/about-jenny-750w.jpg">
+                    <source media="(min-width: 750px)" srcset="images/about-jenny-1000w.jpg">
+                    <img src="images/about-jenny-750w.jpg" alt="desk with monitor and laptop">
+                </picture>
+
+                <div class="about-text-container">
+                    <h3> A little about me</h3>
+                    <p> I am a Front-End Developer and UX/UI Designer with a professional background in graphic design. 
+                    I love being part of the creative process and seeing a well planned project come to life. </p>
+                    <p> In my spare time, you will find me either behind one of my sewing machines creating something 
+                    new or exploring our beautiful landscape on my bicycle. </p>                
+                </div>
+            </section>
+
+            <article class="about-btn">
+                <a href="about.php" class="more-about">More about me</a>
             </article>
 
-        </section>
-
-        <section class="about-jenny-container">
-
-            <picture>
-                <source media="(max-width: 500px)" srcset="images/about-jenny-320w.jpg">
-                <source media="(max-width: 750px)" srcset="images/about-jenny-750w.jpg">
-                <source media="(min-width: 750px)" srcset="images/about-jenny-1000w.jpg">
-                <img src="images/about-jenny-750w.jpg" alt="desk with monitor and laptop">
-			</picture>
-
-            <div class="about-text-container">
-                <h3> A little about me</h3>
-                <p> I am a Front-End Developer and UX/UI Designer with a professional background in graphic design. 
-                I love being part of the creative process and seeing a well planned project come to life. </p>
-                <p> In my spare time, you will find me either behind one of my sewing machines creating something 
-                new or exploring our beautiful landscape on my bicycle. </p>                
-            </div>
-        </section>
-
-        <article class="about-btn">
-            <a href="about.php" class="more-about">More about me</a>
-        </article>
-
-        <section class="index-box-cont">
-            <div class="yellow-box"><p>Home</p></div>
-            <div class="black-box"><p>Go to projects</p></div>
-        </section>
-
+            <section class="index-box-cont">
+                <div class="yellow-box"></div>
+                <div class="black-box"></div>
+            </section>
+    </div> <!-- end wrapper desktop  -->
+    
         <button onclick="topFunction()" id="myBtn" class="top-btn" title="Go to top"><i class="fas fa-arrow-circle-up"></i> </button>
 
         <footer>
