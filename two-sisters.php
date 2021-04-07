ae85ced5bffd8ee31bd0a52ae55f75ebf2253e8d
jennyesc <71670394+jennyesc@users.noreply.github.com>
2021-04-07 10:52:01 -0700

    Update GitHub url for two-sisters

--- two-sisters.php
+++ two-sisters.php
@@ -35,7 +35,7 @@
     
                 <div class="button-container">                   
                     <a class="btn ts-btn" href="https://www.jennyescobell.com/twosisters" target="_blank" rel="noopener noreferrer" > <i class="fas fa-desktop"></i>&nbsp;&nbsp; Live Site</a>
-                    <a class="btn ts-btn" href="https://github.com/htpwebdesign/two-sisters" target="_blank" rel="noopener noreferrer"> <i class="fab fa-github"></i>&nbsp;&nbsp;GitHub</a>
+                    <a class="btn ts-btn" href="https://github.com/jennyesc/two-sisters" target="_blank" rel="noopener noreferrer"> <i class="fab fa-github"></i>&nbsp;&nbsp;GitHub</a>
                 </div>
             </div>
 
